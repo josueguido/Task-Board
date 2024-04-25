23c5d0db4ceef9a9e1d58da615067bd383b616da
Your Name <you@example.com>
2024-04-24 20:11:20 -0600

    Agregada funcionalidad para actualizar automáticamente la vista al borrar datos

--- src/Components/Contexto.jsx
+++ src/Components/Contexto.jsx
@@ -5,20 +5,24 @@ const LocalStorageContext = createContext();
 export const LocalStorageProvider = ({ children }) => {
   const [localStorageData, setLocalStorageData] = useState(() => {
     const storedData = localStorage.getItem('formData');
-    return storedData ? JSON.parse(storedData) : [];
+    return storedData ? JSON.parse(storedData) : {};
   });
 
   const updateLocalStorageData = (newData) => {
-    setLocalStorageData(prevData => {
-        const updatedData = { ...prevData, [newData.id]: newData }; // Estructurar la nota con su ID
-        localStorage.setItem('formData', JSON.stringify(updatedData));
-        return updatedData;
+    setLocalStorageData((prevData) => {
+      const updatedData = { ...prevData, [newData.id]: newData };
+      localStorage.setItem('formData', JSON.stringify(updatedData));
+      return updatedData;
     });
-};
-  
+  };
+
+  const deleteAllLocalStorageData = () => {
+    localStorage.clear();
+    setLocalStorageData({}); // Eliminar todas las notas y forzar un renderizado
+  };
 
   return (
-    <LocalStorageContext.Provider value={{ localStorageData, updateLocalStorageData }}>
+    <LocalStorageContext.Provider value={{ localStorageData, updateLocalStorageData, deleteAllLocalStorageData }}>
       {children}
     </LocalStorageContext.Provider>
   );
--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -1,29 +1,20 @@
-import Add from "../assets/Iconos/Add_round_duotone.svg"
-import { Link } from "react-router-dom"
+import Add from "../assets/Iconos/Add_round_duotone.svg";
+import { Link } from "react-router-dom";
 import { useLocalStorage } from './Contexto';
-import { useState } from "react"
-
 
 const Principal = () => {
-
-    const { localStorageData, updateLocalStorageData } = useLocalStorage();
-    const [selectedNote, setSelectedNote] = useState(null);
-
-    const handleNoteClick = (index) => {
-        setSelectedNote(localStorageData[index]);
-    };
+    const { localStorageData, deleteAllLocalStorageData } = useLocalStorage();
 
     const handleDeleteAll = () => {
-        localStorage.clear();
-        ([]); // Eliminar todas las notas
+        const isConfirmed = window.confirm("¿Estás seguro de que deseas eliminar todas las notas?");
+        if (isConfirmed) {
+            deleteAllLocalStorageData(); // Llamar a la función para eliminar todas las notas
+        }
     };
-
-
 
     return (
         <>
             <article className="flex flex-col items-center justify-center py-4 gap-10">
-
                 <Link to="/Detalles" className="flex flex-row bg-orange-200 py-4 w-5/12 h-18 rounded-lg items-center justify-between px-6">
                     <div className="flex flex-1 items-start gap-10">
                         <div className="flex bg-yellow-500 rounded-md h-10 w-10 justify-center">
@@ -33,66 +24,35 @@ const Principal = () => {
                     </div>
                 </Link>
 
-
-                {/* <div className="flex flex-col gap-4">
-                    {localStorageData.length > 0 ? (
-                        localStorageData.slice(0).reverse().map((task, index) => (
-                            task && task.icon ? ( 
-                            <div key={index} onClick={() => handleNoteClick(index)}>
-                             <Link to={{ pathname: "/Detalles", state: { selectedNote: task } }} className="flex flex-row bg-gray-200 py-4 h-8/12 rounded-lg items-center justify-between px-6">
-                                        <div className="flex items-start gap-10">
-                                            <div className="flex bg-white rounded-md h-10 w-10">
-                                                <p className="flex justify-center text-3xl">{task.icon}</p>
-                                            </div>
-                                            <div className="flex flex-col">
-                                                <h1 className="flex justify-start text-center font-semibold text-2xl">{task.taskName}</h1>
-                                                <p>{task.description}</p>
-                                            </div>
-                                            <div className="flex flex-col justify-center items-center text-center">
-                                                <h2>{task.status}</h2>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                    <button onClick={() => handleNoteClick(index)}>Edit</button>
-                                </div>
-                             ) : null
-                        ))
-                    ) : (
-                        <p>No hay notas guardadas.</p>
-                    )}
-                </div> */}
                 <div className="flex flex-col gap-4">
-                    {/* Itera sobre las notas y muestra cada una */}
                     {Object.values(localStorageData).map(note => (
-                        <div key={note.id} className="bg-gray-200 py-4 h-8/12 rounded-lg">
-                            <Link to={{ pathname: "/Detalles", state: { selectedNote: note } }} className="flex flex-row items-center justify-between px-6">
-                                <div className="flex items-start gap-10">
-                                    <div className="flex bg-white rounded-md h-10 w-10">
-                                        <p className="flex justify-center text-3xl">{note.icon}</p>
+                        note && note.icon ? ( 
+                            <div key={note.id} className="bg-gray-200 py-4 h-8/12 rounded-lg">
+                                <Link to={{ pathname: "/Detalles", state: { selectedNote: note } }} className="flex flex-row items-center justify-between px-6">
+                                    <div className="flex items-start gap-10">
+                                        <div className="flex bg-white rounded-md h-10 w-10">
+                                            <p className="flex justify-center text-3xl">{note.icon}</p>
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <h1 className="flex justify-start text-center font-semibold text-2xl">{note.taskName}</h1>
+                                            <p>{note.description}</p>
+                                        </div>
+                                        <div className="flex flex-col justify-center items-center text-center">
+                                            <h2>{note.status}</h2>
+                                        </div>
                                     </div>
-                                    <div className="flex flex-col">
-                                        <h1 className="flex justify-start text-center font-semibold text-2xl">{note.taskName}</h1>
-                                        <p>{note.description}</p>
-                                    </div>
-                                    <div className="flex flex-col justify-center items-center text-center">
-                                        <h2>{note.status}</h2>
-                                    </div>
-                                </div>
-                            </Link>
-                        </div>
+                                </Link>
+                            </div>
+                        ) : null
                     ))}
                 </div>
 
                 <div className="flex flex-row py-10 gap-4">
                     <button onClick={handleDeleteAll} className="w-28 h-10 rounded-xl bg-red-500 text-white">Delete All</button>
                 </div>
-
             </article>
-
         </>
     );
-
-
 }
 
 export default Principal;
